260d79e1400056d46e121f153c616bf913f04151
imildositoe <imildositoe@gmail.com>
2021-07-07 16:21:33 +0200

    initial commit

---
+++ src/regex/Exemplos.java
@@ -0,0 +1,38 @@
+package regex;
+
+public class Exemplos {
+    /*
+    * Modificadores
+    *   (?i) -- Procura a ocorrência ignorando maiúsculas e minúsculas
+    *   (?x) -- Comentários
+    *   (?m) -- Multilinhas
+    *   (?s) -- Dottal
+    *   */
+
+
+    /*
+    * Metacaracteres
+    *   .   -- Procura a ocorrência de qualquer tipo de caracter
+    *   \\d  -- dígitos  [0-9]
+    *   \\D  -- não é dígito  [^0-9]
+    *   \\s  -- espaços,tabulação,recuo   [\t \n \x0B \f \r]
+    *   \\S  -- não é espaço   [^s]
+    *   \\w  --letra   [a-zA-Z_0-9]
+    *   \\W  --não é letra   [^w]
+    *   */
+
+
+    /*
+    *Quantificadores
+    *   X{n} -- Procura a ocorrência de uma expressão regular "X" EXACTAMENTE N vezes
+    *   X{n,} -- Pelo menos N vezes
+    *   X{n,m} -- No intervalo de M a N vezes
+    *   X?  -- 0 ou 1 vez
+    *   X*  -- 0 ou mais vezes
+    *   X+  -- 1 ou mais vezes
+    *   */
+
+
+    /*
+    * Metacaracteres de Fronteira*/
+}
---
+++ src/regex/Regex.java
@@ -0,0 +1,28 @@
+package regex;
+
+public class Regex {
+    public static void main(String[] args) {
+        boolean e;
+
+        //Modificadores
+        String s="Imildo";
+        boolean b=s.matches("Imildo"); //return TRUE
+        boolean c=s.matches("imildo"); //return FALSE
+        boolean d=s.matches("(?i)imildo"); //return TRUE(Match Ignoring Case)
+
+        //Metacaracteres
+        e = "#".matches("."); //return TRUE
+        e = "3 ".matches("\\d\\s"); //return TRUE [Se forem 5 dígitos tem que por 5 regex's]
+
+        //Quantificadores
+        e = "12".matches("\\d{2}"); //return TRUE (Search 2 ocorrences)
+        e = "12345".matches("\\w{2,5}");
+        e = "ABCD".matches(".*"); // 0 ou +
+
+        //Validar e-mail
+        e = "poisonshade@gmail.com".matches("\\w{3,}@(?i)\\w{1,5}mail.co.+");
+        //Validar data
+        e="29/06/1810".matches("\\d{2}/\\d{2}/\\d{4}");
+        System.out.println(e);
+    }
+}
